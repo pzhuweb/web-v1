3fada299db65ea7f2e0cd51b4ff379d83421d82d
liuxing <723741779@qq.com>
2019-05-09 10:58:03 +0800

    fix: init

--- app/lib/config.js
+++ app/lib/config.js
@@ -3,8 +3,8 @@ const config = {
     database: {
         DATABASE: "web",//数据库
         USERNAME: "root",//用户名
-        PASSWORD: "123456",//密码
-        PORT: "3306",//端口
+        PASSWORD: 123456,//密码
+        PORT: 3306,//端口
         HOST: "mysql" //ip地址
     }
 }
